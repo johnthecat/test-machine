1c2072337b1140ab5deffa8d627b6d3af50d5e86
Sergey Zhuravlev <sergey.zhuravlev@ncloudtech.ru>
2017-04-23 03:32:32 +0300

    Added noModuleException test in plugins package

---
+++ packages/test-machine-plugins/test/utils.spec.ts
@@ -0,0 +1,15 @@
+/// <reference path="../../../node_modules/@types/mocha/index.d.ts" />
+
+import * as chai from 'chai';
+import {noModuleException} from '../src/utils';
+
+describe('Utils', () => {
+    context('noModuleException', () => {
+        it('should return Error', () => {
+            const error = noModuleException('myModule');
+
+            chai.expect(error).to.be.instanceof(Error);
+            chai.expect(error.message).to.includes('myModule');
+        });
+    });
+});
