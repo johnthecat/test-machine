dc260a2505c5e8f5ccb9748a311a461c70f5c170
johnthecat <sergey.zhuravlev@ncloudtech.ru>
2017-12-03 19:08:24 +0300

    Fixed editorconfig, changed noModuleException message

--- packages/test-machine-plugins/src/utils.ts
+++ packages/test-machine-plugins/src/utils.ts
@@ -1,6 +1,12 @@
 export function noModuleException(moduleName: string): Error {
-    return new Error([
-        'Can\'t find ' + moduleName + ' module.',
-        'Install it manually by calling "npm install ' + moduleName + ' --save-dev" inside your terminal.'
-    ].join('\n'));
+    const message = [
+        `Can\'t find ${moduleName} module.`,
+        `Install it manually by calling`,
+        `=> npm install ${moduleName} --save-dev`,
+        'or',
+        `=> yarn add ${moduleName} --dev`,
+        'inside your terminal.'
+    ];
+
+    return new Error(message.join('\n'));
 }
